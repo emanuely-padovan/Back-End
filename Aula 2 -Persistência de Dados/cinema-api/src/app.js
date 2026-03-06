623eb575291fd2270e63b1a24afaa269ebe305db
emanuely-padovan <emanuely.padovan@aluno.senai.br>
2026-03-06 15:52:52 -0300

    Finalização do CRUD

--- Aula 2 -Persistência de Dados/cinema-api/src/app.js
+++ Aula 2 -Persistência de Dados/cinema-api/src/app.js
@@ -69,4 +69,137 @@ app.get('/filmes/:id', async(req, res) => {
     }
 })
 
+// Continuação do CRUD - 06/03
+app.post('/filmes', async(req, res) => {
+    try {
+        const {titulo, genero, duracao, classificacao, data_lancamento} = req.body
+        if (!titulo || !genero || !duracao) { // Obs.: || titulo !== ' '
+            return res.status(400).json({
+                sucesso: false,
+                mensagem: "Título, gênero e duração são itens obrigatórios!"
+            })
+        }
+
+        if (typeof duracao !== 'number' || duracao <= 0) {
+            return res.status(400).json({
+                sucesso: false,
+                mensagem: "A duração do filme deve ser um número positivo..."
+            })
+        }
+
+        const novoFilme = {
+            titulo: titulo.trim(),
+            genero: genero.trim(),
+            duracao: duracao,
+            classificacao: classificacao || null,
+            data_lancamento: data_lancamento || null
+        }
+
+        const resultado = await queryAsync ("INSERT INTO filme SET ?", [novoFilme]) // Atribuição de valores
+        res.status(201).json({
+            sucesso: true, 
+            mensagem: "Filme criado com sucesso!",
+            id: resultado.insertId
+        })
+    } catch (erro) {
+        console.error("Erro ao inserir filme: ", erro)
+        res.status(500).json({
+            sucesso: false,
+            mensagem: "Erro ao inserir filme!",
+            erro: erro.message
+        })
+    }
+})
+
+app.put('/filmes/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const {titulo, genero, duracao, classificacao, data_lancamento} = req.body
+
+        if (!id || isNaN(id)) {
+            return res.status(400).json({
+                sucesso: false,
+                mensagem: "ID de filme inválido!"
+            })
+        }
+
+        const filmeExiste = await queryAsync ("SELECT * FROM filme WHERE id = ?", [id])
+        if (filmeExiste.length === 0) {
+            return res.status(404).json({
+                sucesso: false,
+                mensagem: "Filme não encontrado!"
+            })
+        }
+
+        const filmeAtualizado = {}
+
+        if (titulo !== undefined) filmeAtualizado.titulo = titulo.trim()
+        if (genero !== undefined) filmeAtualizado.genero = genero.trim()
+        if (duracao !== undefined) {
+            if (typeof duracao !== 'number'|| duracao <= 0 )
+                return res.status(400).json({
+                    sucesso: false,
+                    mensagem: "Duração deve ser um número positivo..."
+            })
+            filmeAtualizado.duracao = duracao
+        }
+        if (classificacao !== undefined) filmeAtualizado.classificacao = classificacao.trim()
+        if (data_lancamento !== undefined) filmeAtualizado.data_lancamento = data_lancamento.trim()
+
+        if (Object.keys (filmeAtualizado).length === 0) {
+            return res.status(400).json({
+                sucesso: false, 
+                mensagem: "Nenhum campo para atualizar..."
+            })
+        }
+
+        await queryAsync ("UPDATE filme SET ? WHERE id = ?", [filmeAtualizado, id])
+        res.json({
+            sucesso: true, 
+            mensagem: "Filme atualizado!"
+        })
+    } catch (erro){
+        console.error("Erro ao atualizar filme: ", erro)
+        res.status(500).json({
+            sucesso: false,
+            mensagem: "Erro ao atualizar filme!",
+            erro: erro.message
+        })
+    }
+})
+
+app.delete('/filmes/:id', async(req, res) => {
+    try {
+        const {id}= req.params // Encontrar o Id do filme que eu quero apagar
+
+        if (!id || isNaN(id)) {
+            return res.status(400).json({
+                sucesso: false,
+                mensagem: "ID de filme inválido!"
+            })
+        }
+
+        const filmeExiste = await queryAsync ("SELECT * FROM filme WHERE id = ?", [id])
+        if (filmeExiste.length === 0) { // A busca não retornou em nada...
+            return res.status(404).json({
+                sucesso: false,
+                mensagem: "Filme não encontrado!"
+            })
+        }
+
+        await queryAsync ("DELETE FROM filme WHERE id = ?", [id])
+        res.json({
+            sucesso: true,
+            mensagem: "Filme foi apagado com sucesso!🎉"
+        })
+    } catch (erro) {
+        console.error("Erro ao deletar o filme: ", erro)
+        res.status(500).json({
+            sucesso: false,
+            mensagem: "Erro ao deletar o filme!",
+            erro: erro.message
+        })        
+    }
+})
+
 module.exports = app // Garante que eu consiga acessar o app em outros arquivos, como o server.js
